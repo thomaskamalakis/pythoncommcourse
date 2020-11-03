74c3374cccf29c84d57d2997063e8aafdc6da07a
Thomas Kamalakis <thkam@hua.gr>
2020-11-03 19:11:47 +0200

    changes on november 3rd lecture

--- lecture2/fftexample.py
+++ lecture2/fftexample.py
@@ -2,9 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # function to create the square pulse
-def square(t , T):
-    
-    f = np.logical_and( t <= T / 2.0 , t >= -T/2.0 )
+def square(t1 , T1):  
+    f = np.logical_and( t1 <= T1 / 2.0 , t1 >= -T1/2.0 )
     return f.astype('float')
      
 # time axis
@@ -20,23 +19,25 @@ Df = 1.0 / ( N * Dt)
 f = n * Df
 
 # signal in the time domain
-x=square(t,T)
-
+x=square(t, T)
+#
 # signal in the frequency domain
-X=Dt*np.fft.fftshift(np.fft.fft(np.fft.fftshift(x)))
+X = Dt * np.fft.fftshift( np.fft.fft( np.fft.fftshift(x) ) )
 
 # Analytical expression for the spectrum
 X2= T * np.sinc( f * T )
 
 # Plot results
+plt.close('all')
+
 plt.figure(1)
 plt.xlabel('t [msec]')
 plt.ylabel('x(t)')
 plt.plot(t,x)
 
 plt.figure(2)
-plt.plot(t,np.real(X2),t,X,'o')
-plt.xlim(-3,3)
+plt.plot(f, X2 , f , np.real(X), 'o')
+plt.xlim(-1.5, 1.5)
 plt.legend(['analytical','numerical'])
 plt.xlabel('f [kHz]')
 plt.ylabel('X(f)')
